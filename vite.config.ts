5629f5eb7f484450d5fd37203a6000306d93abcc
Frank Feng <frankfuse@hotmail.com>
2026-06-06 06:34:00 +0800

    dns save

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig(({ mode }) => ({
-  base: mode === 'ghpages' ? '/gamehub/' : '/',
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
